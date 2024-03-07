12bcd6a22e61e560bf5630a763872a5ade97f4c7
Aaron-bukasa <01bukasa@gmail.com>
2024-03-07 17:38:13 +0200

    ajout react vite et tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
+      colors: {
+        grayColor: '#484873',
+        graySecond: '#e6e6e6',
+        grayOpacity: '#48487394',
+        redColor: '#e76f51',
+        yellowColor: '#d9a703',
+      },
+      fontFamily: {
+        'lora': 'Lora, serif',
+        'roboto': 'Roboto, sans-serif'
+      },
+      content: {
+        'linkAfter': 'url("./src/assets/img/expand_more.svg")',
+      },
+    },
+  },
+  plugins: [],
+}
