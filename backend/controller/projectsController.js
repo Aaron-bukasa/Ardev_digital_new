43434fe364e700980af37bd88c9df66c1d950bdb
Aaron-bukasa <01bukasa@gmail.com>
2024-03-13 00:25:11 +0200

    ajout de la base des données et intégrqtion du prisma

--- backend/controller/projectsController.js
+++ backend/controller/projectsController.js
@@ -1,21 +1,22 @@
 const express = require('express');
+const jsonData = require('../public/portfolio.json');
 
 exports.projectsAll = function(req, res, next) {
-    
+    res.render('projectsAll', { jsonData });
 };
 
 exports.projectsAdd = function(req, res, next) {
-    
+    res.status(200).json({message: "la route n'est pas encore inplementée"})
 };
 
 exports.projectsDetail = function(req, res, next) {
-    
+    res.status(200).json({message: "la route n'est pas encore inplementée"})
 };
 
 exports.projectsDelete = function(req, res, next) {
-    
+    res.status(200).json({message: "la route n'est pas encore inplementée"})
 };
 
 exports.projectsUpdate = function(req, res, next) {
-    
+    res.status(200).json({message: "la route n'est pas encore inplementée"})
 };
--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -4,7 +4,6 @@ const ctr_projects = require('../controller/projectsController');
 
 router.get('/', ctr_projects.projectsAll);
 router.post('/', ctr_projects.projectsAdd)
-router.get('/id', ctr_projects.projectsDetail)
 router.delete('/id', ctr_projects.projectsDelete)
 router.put('/id', ctr_projects.projectsUpdate)
 
